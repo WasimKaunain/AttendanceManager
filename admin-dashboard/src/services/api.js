cb5f96843bf15cdd42de0683403b94c1bbe75a4c
WasimKaunain <ansariw580@gmail.com>
2026-02-22 02:24:04 +0530

    updated frontend part

---
+++ admin-dashboard/src/services/api.js
@@ -0,0 +1,13 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const loginUser = async (data) => {
+  const response = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+};
