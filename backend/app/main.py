723b6a32f29eabfcb5767202650c8d6de06f2830
WasimKaunain <ansariw580@gmail.com>
2026-02-22 05:02:26 +0530

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://attendance-manager-vert.vercel.com"
+        "https://attendance-manager-vert.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
