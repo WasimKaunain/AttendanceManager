0ae552bd43a36ff65b94021dec0e54f3e66ff251
WasimKaunain <ansariw580@gmail.com>
2026-03-01 03:40:19 +0530

    resolved site update bug

--- admin-dashboard/src/modules/sites/services.js
+++ admin-dashboard/src/modules/sites/services.js
@@ -4,7 +4,7 @@ export const getSites = async () => { const res = await api.get("/sites/"); retu
 
 export const createSite = async (data) => { const res = await api.post("/sites/", data); return res.data; };
 
-export const updateSite = async ({ id, data }) => { const res = await api.put(`/sites/${id}`, data); return res.data; };
+export const updateSite = async ({ id, data }) => { const res = await api.patch(`/sites/${id}`, data); return res.data; };
 
 export const deleteSite = async (id) => { await api.delete(`/sites/${id}`); };
 
