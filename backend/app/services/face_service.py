2aad73186597fafef9fa9f73ad846242c32a5a46
WasimKaunain <ansariw580@gmail.com>
2026-03-02 00:07:05 +0530

    updated threshold value

--- backend/app/services/face_service.py
+++ backend/app/services/face_service.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def cosine_similarity(a: list[float], b: list[float]) -> float:
     a = np.array(a)
@@ -6,7 +7,7 @@ def cosine_similarity(a: list[float], b: list[float]) -> float:
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
-def is_same_person(emb1: list[float],emb2: list[float],threshold: float = 0.75) -> bool:
+def is_same_person(emb1: list[float],emb2: list[float],threshold: float = os.getenv('THRESHOLD')) -> bool:
 
     if len(emb1) != len(emb2):
         return False
